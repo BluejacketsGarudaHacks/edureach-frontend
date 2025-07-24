3c4cb0eacb39966b2ae915c3dd87c2013a201963
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-07-24 23:09:14 +0700

    feat: add routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,18 @@
-import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-    index("routes/pages/welcome.tsx"),
-    ...prefix("/auth", [
-        route("/login", "routes/pages/login/page.tsx"),
-        route("/register", "routes/pages/register/page.tsx")
-    ]),
-    route("/home", "routes/pages/home/page.tsx"),
-    route("/create-community", "routes/pages/create-community/page.tsx"),
-    route("/profile", "routes/pages/profile/page.tsx"),
+  index("routes/pages/welcome.tsx"),
+  ...prefix("/auth", [
+    route("/login", "routes/pages/login/page.tsx"),
+    route("/register", "routes/pages/register/page.tsx"),
+  ]),
+  route("/home", "routes/pages/home/page.tsx"),
+  route("/create-community", "routes/pages/create-community/page.tsx"),
+  route("/profile", "routes/pages/profile/page.tsx"),
+  route("/summarizer", "routes/pages/summarizer/summarizer-page.tsx"),
 ] satisfies RouteConfig;
