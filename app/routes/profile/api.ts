215da949e3fdfa08ef75549082a92664cacce22e
axelkrnwn <axelkurn@gmail.com>
2025-07-25 15:21:31 +0700

    Feat(Profile): Update formdata for update profile

--- app/routes/profile/api.ts
+++ app/routes/profile/api.ts
@@ -28,13 +28,15 @@ export const updateUser = ({data, token}:{data:updateUserInput, token: string}) 
     const formData = new FormData()
 
     if (data.fullName.split(' ').length >= 2){
-        formData.append("FirstName", data.fullName.split(' ')[0])
-        formData.append("LastName", data.fullName.split(' ').length < 2 ? data.fullName.split(' ')[0]: data.fullName.split(' ')[1])
+        let names =  data.fullName.split(' ')
+        formData.append("FirstName",names[0])
+        formData.append("LastName", names[names.length - 1])
     }else{
         formData.append("FirstName", data.fullName)
     }
     formData.append("Dob", data.dateOfBirth)
     formData.append("Email", data.email)
+
     if (data.image){
         formData.append("Image", data.image)
     }
